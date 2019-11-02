9a4a0eeb9724c5af3a0a712d7c5b50c627ea9a69
GoodGuyGregory <greg.witt625@gmail.com>
2019-11-02 17:31:49 -0400

    reconfigures firebase with npm dependency injection

---
+++ clientform/src/firebase/init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCPym2WqUGrMU6tMY0O2qp7Y3HcBP1Yt0g",
+    authDomain: "nursingdb-test.firebaseapp.com",
+    databaseURL: "https://nursingdb-test.firebaseio.com",
+    projectId: "nursingdb-test",
+    storageBucket: "nursingdb-test.appspot.com",
+    messagingSenderId: "926234803998",
+    appId: "1:926234803998:web:1b9cdcad765c8fed0c530f",
+    measurementId: "G-CTKE6YMR9X"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
