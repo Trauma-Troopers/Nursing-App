e41fcf84a279bfb6ca84ea554a441f5d507c72f6
GoodGuyGregory <greg.witt625@gmail.com>
2020-02-17 01:59:06 -0500

    adds auth restrictions and creates Checklist Component

---
+++ clientform/src/components/layout/CheckList.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <table class="table">
+      <tr>
+        <td v-for="item in tabs" @click="filterChecks(item.id)">{{item.id}}</td>
+      </tr>
+      <!-- tabs -->
+      <tr>
+        <td colspan="3">Name:</td>
+        <td colspan="3">Level 1</td>
+        <td colspan="3">Level 2</td>
+        <td colspan="3">Level 3</td>
+        <td colspan="3">Level 4</td>
+      </tr>
+      <tr v-for="check in checks">
+        <td colspan="3">{{check.get("name")}}</td>
+        <td colspan="3">
+          <input type="checkbox" v-model="checked1" />
+        </td>
+        <td colspan="3">
+          <input type="checkbox" v-model="checked2" />
+        </td>
+        <td colspan="3">
+          <input type="checkbox" v-model="checked3" />
+        </td>
+        <td colspan="3">
+          <input type="checkbox" v-model="checked4" />
+        </td>
+      </tr>
+      <!-- checks row -->
+    </table>
+  </div>
+</template>
+
+
+
+<script>
+import firebase from "@/firebase/init";
+export default {
+  name: 'CheckList',
+    components: {
+        firebase,
+    },
+    data: function () {
+        return {
+            tabs: [],
+            checks: [],
+            tabnum: 0,
+            checked1: [],
+            checked2: [],
+            checked3: [],
+            checked4: []
+        }
+    },
+    methods: {
+        loadPage: function () {
+            firebase.collection("checklist").get().then((querySnapshot) => {
+                this.tabnum = (querySnapshot.size) / 5
+                querySnapshot.forEach((doc) => {
+                    this.tabs.push(doc)
+                });
+            });
+            this.filterChecks("General")
+        },
+        filterChecks: function (id) {
+            this.checks = []
+            var checkitems = firebase.collection("checklist").doc(id).collection("checkitems")
+            checkitems.get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    this.checks.push(doc)
+                    this.checked1.push(doc.get("level1"))
+                    this.checked2.push(doc.get("level2"))
+                    this.checked3.push(doc.get("level3"))
+                    this.checked4.push(doc.get("level4"))
+                })
+            })
+        }
+    },
+    beforeMount() {
+        this.loadPage()
+    }
+  
+}
+</script>
+
+<style>
+</style>
--- clientform/src/router/index.js
+++ clientform/src/router/index.js
@@ -1,21 +1,48 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/auth/Login'
-// import SignUp Component
+// Imports Signup
 import Signup from '@/components/auth/Signup'
+//  imports login 
+import Login from '@/components/auth/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  // changes those slashed inside of the main URL
+  mode: 'history',
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: Login
-    }, {
       path: '/signup',
       name: 'Signup',
       component: Signup
+    }, {
+      path: '/',
+      name: 'Login',
+      component: Login
     }
   ]
 })
+
+// Creates Routeguard
+// fires a function before each route is loaded into the DOM
+
+router.beforeEach((to, from, next) => {
+  // checks the routes for Metadata properites
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check the auth state of the user
+    let user = firebase.auth().currentUser
+    // check and see if the user exisits
+    if (user) {
+      // user is signed in, allow proceeding to the route
+      next()
+    } else {
+      // not signed in, redirect to login page
+      next({ name: 'Login' })
+    }
+  } else {
+    next()
+  }
+})
+
+// Exports the router object for other components
+export default router
