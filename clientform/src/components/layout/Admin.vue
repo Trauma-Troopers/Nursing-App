eea0fa394965fbc169f68c0bb11597e35f843c36
Daniel Russell <daniel.russell@e-hps.com>
2020-04-29 14:29:56 -0400

    Added alert to admin page

--- clientform/src/components/layout/Admin.vue
+++ clientform/src/components/layout/Admin.vue
@@ -123,28 +123,36 @@ export default {
   },
   methods: {
       populate() {         
-        // slugify always takes two params
-        this.slug = slugify(this.username, {
-          replacement: "-",
-          //   regex globals
-          remove: /[$*_+~.()'"!\-:@]/g,
-          lower: true
-        });
-        // let ref is going to equal an object from the users collection
-        // .doc will reference the slug passed as the id
-        let ref = db.collection("users").doc(this.slug)
-        // Check to determine if the reference exists
-        
-        db.collection("users").where("username", "==", this.slug).get()
-        .then((userSnap) => {
-          this.user = userSnap.docs[0].ref
-          this.user.collection("checklist").get().then((querySnapshot) => {
-              querySnapshot.forEach((doc) => {
-                  this.tabs.push(doc)
-              });
+        if (this.username){
+          // slugify always takes two params
+          this.slug = slugify(this.username, {
+            replacement: "-",
+            //   regex globals
+            remove: /[$*_+~.()'"!\-:@]/g,
+            lower: true
           });
-            this.filterChecks("General")
-        })
+          // let ref is going to equal an object from the users collection
+          // .doc will reference the slug passed as the id
+          let ref = db.collection("users").doc(this.slug)
+          // Check to determine if the reference exists
+          ref.get().then(doc => {
+            if (doc.exists) {
+              db.collection("users").where("username", "==", this.slug).get()
+              .then((userSnap) => {
+                this.user = userSnap.docs[0].ref
+                this.user.collection("checklist").get().then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        this.tabs.push(doc)
+                    });
+                });
+              this.filterChecks("General")
+              })
+              this.feedback = '';
+            } else {
+              this.feedback = `User '${this.username}' does not exist, please try again.`;
+            }
+          })
+        } 
       },
       filterChecks: function (id) {
         //this.backgroundColor = 800000 // attempt to dynamically change the tab color
